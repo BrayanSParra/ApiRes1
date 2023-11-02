a2805dd477f758034341d781b93aa9c80c1886c7
Brayan Steven Parra Buriticá <bstevnenparra20@gmail.com>
2023-11-02 07:42:44 -0500

    asignacion de puerto

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ let express = require('express');
 let mysql = require('mysql');
 let app = express();
 app.use(express.json());
+
+app.listen('3000', function(){
+  console.log('Servidor OK','3000');
+})
